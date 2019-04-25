a7a80ea9c809eea0d5ca3f67b7286b351d92a1c4
Echeney17 <48723154+Echeney17@users.noreply.github.com>
2019-04-24 21:08:04 -0400

    Initial commit

---
+++ src/com/company/Inventory.java
@@ -0,0 +1,80 @@
+package com.company;
+
+/*
+Project: Team Project Design: Car Dealership:
+Purpose Details: Inventory Java Class
+Course: IST 242
+Team:  4
+Date Developed: 02/17/2019
+Last Date Changed: 04/24/2019
+Rev: 4
+*/
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+public class Inventory
+{
+    //Inventory
+    private String Car;
+    private String Accessories;
+    private ArrayList<Inventory> Parts;
+
+   /* public Inventory(String _car, String _accessories, String _parts){
+        this.Car = _car;
+        this.Accessories = _accessories;
+        this.Parts = _parts;
+    } */
+
+    //Lists for all Inventory things
+    ArrayList<Inventory> carList = new ArrayList();
+    ArrayList<Inventory> accessoryList = new ArrayList();
+    ArrayList<Inventory> partsList = new ArrayList();
+
+    //Parts List
+    String part1 = "Brakes";
+    //Methods
+    public String getCar() {
+        return Car;
+
+    }
+    /**
+     * @param    The activity context
+     * @param ssid      Wifi Access Point to connect too
+     * @return          Connected true or false
+     * @author          Joe Oakes
+     * @version         1.0
+     * @since           2019-04-08
+     */
+    public void setCar(String _car) {
+        Car = _car;
+
+    }
+
+    public String getAccessories() {
+        return Accessories;
+
+    }
+
+    public void setAccessories(String _accessories) {
+        Accessories = _accessories;
+
+    }
+
+    public String getParts() {
+        return Parts.toString();
+
+    }
+
+    public void setParts(ArrayList<Inventory> Parts) {
+        Parts.toString();
+    }
+
+    public static void printParts(ArrayList<Inventory> partsList)
+    {
+        System.out.println("Parts list: ");
+        for (Inventory part : partsList)
+        {
+            System.out.println(part.part1);
+        }
+    }
+}
